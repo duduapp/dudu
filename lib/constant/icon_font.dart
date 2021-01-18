5fb57cb9e908d923930b112e01ea5f6258fe4a09
VikeyChen <vikeytk@gmail.com>
2021-01-18 16:34:40 +0800

    add icon

--- lib/constant/icon_font.dart
+++ lib/constant/icon_font.dart
@@ -1,113 +1,115 @@
 import 'package:flutter/cupertino.dart';
 
 class IconFont {
-  static const IconData checkCircle = IconData(0xe62b,fontFamily: 'IconFont');
+  static const IconData checkCircle = IconData(0xe62b, fontFamily: 'IconFont');
 
-  static const IconData time = IconData(0xe63d,fontFamily: 'IconFont');
+  static const IconData time = IconData(0xe63d, fontFamily: 'IconFont');
 
-  static const IconData list = IconData(0xe61f,fontFamily: 'IconFont');
+  static const IconData list = IconData(0xe61f, fontFamily: 'IconFont');
 
-  static const IconData message = IconData(0xe60e,fontFamily: 'IconFont');
+  static const IconData message = IconData(0xe60e, fontFamily: 'IconFont');
 
-  static const IconData bookmark = IconData(0xe8ac,fontFamily: 'IconFont');
+  static const IconData bookmark = IconData(0xe8ac, fontFamily: 'IconFont');
 
-  static const IconData settings = IconData(0xe9cb,fontFamily: 'IconFont');
+  static const IconData settings = IconData(0xe9cb, fontFamily: 'IconFont');
 
-  static const IconData accountSetting = IconData(0xe6e2,fontFamily: 'IconFont');
+  static const IconData accountSetting =
+      IconData(0xe6e2, fontFamily: 'IconFont');
 
-  static const IconData search = IconData(0xe671,fontFamily: 'IconFont');
+  static const IconData search = IconData(0xe671, fontFamily: 'IconFont');
 
-  static const IconData mine = IconData(0xe63a,fontFamily: 'IconFont');
+  static const IconData mine = IconData(0xe63a, fontFamily: 'IconFont');
 
-  static const IconData local = IconData(0xe651,fontFamily: 'IconFont');
+  static const IconData local = IconData(0xe651, fontFamily: 'IconFont');
 
-  static const IconData reblog = IconData(0xeef5,fontFamily: 'IconFont');
+  static const IconData reblog = IconData(0xeef5, fontFamily: 'IconFont');
 
-  static const IconData notification = IconData(0xe694,fontFamily: 'IconFont');
+  static const IconData notification = IconData(0xe694, fontFamily: 'IconFont');
 
-  static const IconData addCircle = IconData(0xe787,fontFamily: 'IconFont');
+  static const IconData addCircle = IconData(0xe787, fontFamily: 'IconFont');
 
-  static const IconData home = IconData(0xe9ce,fontFamily: 'IconFont');
+  static const IconData home = IconData(0xe9ce, fontFamily: 'IconFont');
 
-  static const IconData thumbUp = IconData(0xe9cf,fontFamily: 'IconFont');
+  static const IconData thumbUp = IconData(0xe9cf, fontFamily: 'IconFont');
 
-  static const IconData earth = IconData(0xeef6,fontFamily: 'IconFont');
+  static const IconData earth = IconData(0xeef6, fontFamily: 'IconFont');
 
-  static const IconData earthSmall = IconData(0xe61b,fontFamily: 'IconFont'); // use in bottom navigation
+  static const IconData earthSmall =
+      IconData(0xe61b, fontFamily: 'IconFont'); // use in bottom navigation
 
-  static const IconData theme = IconData(0xe6a7,fontFamily: 'IconFont');
+  static const IconData theme = IconData(0xe6a7, fontFamily: 'IconFont');
 
-  static const IconData volumeOff = IconData(0xe69c,fontFamily: 'IconFont');
+  static const IconData volumeOff = IconData(0xe69c, fontFamily: 'IconFont');
 
-  static const IconData copy = IconData(0xe8c9,fontFamily: 'IconFont');
+  static const IconData copy = IconData(0xe8c9, fontFamily: 'IconFont');
 
-  static const IconData eyeClose = IconData(0xe66d,fontFamily: 'IconFont');
+  static const IconData eyeClose = IconData(0xe66d, fontFamily: 'IconFont');
 
-  static const IconData link = IconData(0xe70e,fontFamily: 'IconFont');
+  static const IconData link = IconData(0xe70e, fontFamily: 'IconFont');
 
-  static const IconData fontSize = IconData(0xe837,fontFamily: 'IconFont');
+  static const IconData fontSize = IconData(0xe837, fontFamily: 'IconFont');
 
-  static const IconData www = IconData(0xe638,fontFamily: 'IconFont');
+  static const IconData www = IconData(0xe638, fontFamily: 'IconFont');
 
-  static const IconData picture = IconData(0xe60c,fontFamily: 'IconFont');
+  static const IconData picture = IconData(0xe60c, fontFamily: 'IconFont');
 
-  static const IconData block = IconData(0xe677,fontFamily: 'IconFont');
+  static const IconData block = IconData(0xe677, fontFamily: 'IconFont');
 
-  static const IconData eye = IconData(0xe630,fontFamily: 'IconFont');
+  static const IconData eye = IconData(0xe630, fontFamily: 'IconFont');
 
-  static const IconData unlock = IconData(0xe657,fontFamily: 'IconFont');
+  static const IconData unlock = IconData(0xe657, fontFamily: 'IconFont');
 
-  static const IconData lock = IconData(0xe6ba,fontFamily: 'IconFont');
+  static const IconData lock = IconData(0xe6ba, fontFamily: 'IconFont');
 
-  static const IconData report = IconData(0xe6a9,fontFamily: 'IconFont');
+  static const IconData report = IconData(0xe6a9, fontFamily: 'IconFont');
 
-  static const IconData vote = IconData(0xe632,fontFamily: 'IconFont');
+  static const IconData vote = IconData(0xe632, fontFamily: 'IconFont');
 
-  static const IconData cw = IconData(0xe647,fontFamily: 'IconFont');
+  static const IconData cw = IconData(0xe647, fontFamily: 'IconFont');
 
-  static const IconData emoji = IconData(0xe646,fontFamily: 'IconFont');
+  static const IconData emoji = IconData(0xe646, fontFamily: 'IconFont');
 
-  static const IconData forward = IconData(0xe60b,fontFamily: 'IconFont');
+  static const IconData forward = IconData(0xe60b, fontFamily: 'IconFont');
 
-  static const IconData at = IconData(0xe65c,fontFamily: 'IconFont');
+  static const IconData at = IconData(0xe65c, fontFamily: 'IconFont');
 
-  static const IconData follow = IconData(0xe67c,fontFamily: 'IconFont');
+  static const IconData follow = IconData(0xe67c, fontFamily: 'IconFont');
 
-  static const IconData delete = IconData(0xe65f,fontFamily: 'IconFont');
+  static const IconData delete = IconData(0xe65f, fontFamily: 'IconFont');
 
-  static const IconData volumeUp = IconData(0xe85c,fontFamily: 'IconFont');
+  static const IconData volumeUp = IconData(0xe85c, fontFamily: 'IconFont');
 
-  static const IconData clear = IconData(0xe7c8,fontFamily: 'IconFont');
+  static const IconData clear = IconData(0xe7c8, fontFamily: 'IconFont');
 
-  static const IconData expandMore = IconData(0xe633,fontFamily: 'IconFont');
+  static const IconData expandMore = IconData(0xe633, fontFamily: 'IconFont');
 
-  static const IconData personAdd = IconData(0xe60d,fontFamily: 'IconFont');
+  static const IconData personAdd = IconData(0xe60d, fontFamily: 'IconFont');
 
-  static const IconData more = IconData(0xe628,fontFamily: 'IconFont');
+  static const IconData more = IconData(0xe628, fontFamily: 'IconFont');
 
-  static const IconData download = IconData(0xe610,fontFamily: 'IconFont');
+  static const IconData download = IconData(0xe610, fontFamily: 'IconFont');
 
-  static const IconData share = IconData(0xe622,fontFamily: 'IconFont');
+  static const IconData share = IconData(0xe622, fontFamily: 'IconFont');
 
-  static const IconData back = IconData(0xe715,fontFamily: 'IconFont');
+  static const IconData back = IconData(0xe715, fontFamily: 'IconFont');
 
-  static const IconData moreHoriz = IconData(0xe721,fontFamily: 'IconFont');
+  static const IconData moreHoriz = IconData(0xe721, fontFamily: 'IconFont');
 
-  static const IconData about = IconData(0xe60f,fontFamily: 'IconFont');
+  static const IconData about = IconData(0xe60f, fontFamily: 'IconFont');
 
-  static const IconData edit = IconData(0xe662,fontFamily: 'IconFont');
+  static const IconData edit = IconData(0xe662, fontFamily: 'IconFont');
 
-  static const IconData favorite = IconData(0xe913,fontFamily: 'IconFont');
+  static const IconData favorite = IconData(0xe913, fontFamily: 'IconFont');
 
-  static const IconData video = IconData(0xe6a0,fontFamily: 'IconFont');
+  static const IconData video = IconData(0xe6a0, fontFamily: 'IconFont');
 
-  static const IconData audio = IconData(0xed23,fontFamily: 'IconFont');
+  static const IconData audio = IconData(0xed23, fontFamily: 'IconFont');
 
-  static const IconData discovery = IconData(0xe612,fontFamily: 'IconFont');
+  static const IconData discovery = IconData(0xe612, fontFamily: 'IconFont');
 
-  static const IconData square = IconData(0xe615,fontFamily: 'IconFont');
+  static const IconData square = IconData(0xe615, fontFamily: 'IconFont');
 
-  static const IconData display = IconData(0xe600,fontFamily: 'IconFont');
+  static const IconData display = IconData(0xe600, fontFamily: 'IconFont');
 
-
+  static const IconData translate = IconData(0xeef7, fontFamily: 'IconFont');
 }
--- lib/pages/setting/general_setting.dart
+++ lib/pages/setting/general_setting.dart
@@ -1,11 +1,10 @@
-import 'package:dudu/l10n/l10n.dart';
 import 'package:dudu/constant/icon_font.dart';
+import 'package:dudu/l10n/l10n.dart';
 import 'package:dudu/models/instance/instance_manager.dart';
 import 'package:dudu/models/local_account.dart';
 import 'package:dudu/models/logined_user.dart';
 import 'package:dudu/models/provider/settings_provider.dart';
 import 'package:dudu/pages/home_page.dart';
-import 'package:dudu/pages/login/login.dart';
 import 'package:dudu/pages/setting/about_app.dart';
 import 'package:dudu/pages/setting/account_switch.dart';
 import 'package:dudu/pages/setting/setting_content.dart';
@@ -27,7 +26,7 @@ class _GeneralSettingState extends State<GeneralSetting> {
   int textScale = 1; // 0 , 1 ,2 分别对对应小中大
 
   @override
-  void initState(){
+  void initState() {
     _getTextScale();
     super.initState();
   }
@@ -43,32 +42,33 @@ class _GeneralSettingState extends State<GeneralSetting> {
       textScale = idx;
     });
     Storage.saveInt('mastodon.text_scale', idx);
-    eventBus.emit(EventBusKey.textScaleChanged,idx);
+    eventBus.emit(EventBusKey.textScaleChanged, idx);
   }
 
   _onExitPressed() {
     showModalBottomSheet(
         context: context,
         builder: (_) => Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              BottomSheetItem(
-                text: S.of(context).sign_out,
-                onTap: () => _onConfirmExit(),
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  BottomSheetItem(
+                    text: S.of(context).sign_out,
+                    onTap: () => _onConfirmExit(),
+                  ),
+                  Container(
+                    height: 8,
+                    color: Theme.of(context).backgroundColor,
+                  ),
+                  BottomSheetCancelItem()
+                ],
               ),
-              Container(
-                height: 8,
-                color: Theme.of(context).backgroundColor,
-              ),
-              BottomSheetCancelItem()
-            ],
-          ),
-        ));
+            ));
   }
-  _onConfirmExit() async{
+
+  _onConfirmExit() async {
     await LocalStorageAccount.logout();
     Request.closeHttpClient();
     LoginedUser().logout();
@@ -78,7 +78,9 @@ class _GeneralSettingState extends State<GeneralSetting> {
     } else {
       SettingsProvider().setHomeTabIndex(2);
       SettingsProvider().setCurrentUser(null);
-      AppNavigate.pushAndRemoveUntil(HomePage(logined: false,));
+      AppNavigate.pushAndRemoveUntil(HomePage(
+        logined: false,
+      ));
     }
   }
 
@@ -94,27 +96,45 @@ class _GeneralSettingState extends State<GeneralSetting> {
             providerKey: 'theme',
             leftIcon: Icon(IconFont.theme),
             title: S.of(context).application_theme,
-            options: ['0','1','2'],
-            displayOptions: [S.of(context).normal_mode,S.of(context).dark_mode,S.of(context).darkest_mode],
+            options: ['0', '1', '2'],
+            displayOptions: [
+              S.of(context).normal_mode,
+              S.of(context).dark_mode,
+              S.of(context).darkest_mode
+            ],
             type: SettingType.string,
           ),
           ProviderSettingCell(
             providerKey: 'language',
-            leftIcon: Icon(IconFont.fontSize),
+            leftIcon: Icon(IconFont.earth),
             title: S.of(context).language,
-            options: ['zh','en','fr','ru','ar','es','ja','de'],
-            displayOptions: ['中文','English','français','русский','العربية','	Español','日本語','German'],
+            options: ['zh', 'en', 'fr', 'ru', 'ar', 'es', 'ja', 'de'],
+            displayOptions: [
+              '中文',
+              'English',
+              'français',
+              'русский',
+              'العربية',
+              '	Español',
+              '日本語',
+              'German'
+            ],
             type: SettingType.string,
           ),
           ProviderSettingCell(
             providerKey: 'text_scale',
             leftIcon: Icon(IconFont.fontSize),
             title: S.of(context).font_size,
-            options: ['0','1','2'],
-            displayOptions: [S.of(context).small,S.of(context).medium,S.of(context).big],
+            options: ['0', '1', '2'],
+            displayOptions: [
+              S.of(context).small,
+              S.of(context).medium,
+              S.of(context).big
+            ],
             type: SettingType.string,
           ),
           SettingCell(
+            leftIcon: Icon(IconFont.translate),
             title: S.of(context).translate_setting,
             onPress: () => AppNavigate.push(SettingTranslate()),
           ),
@@ -123,23 +143,30 @@ class _GeneralSettingState extends State<GeneralSetting> {
             title: S.of(context).display_setting,
             onPress: () => AppNavigate.push(SettingContent()),
           ),
-
-
           SettingCell(
             leftIcon: Icon(IconFont.about),
-    title: S.of(context).about_dudu,
-
+            title: S.of(context).about_dudu,
             onPress: () => AppNavigate.push(AboutApp()),
           ),
-
-          SizedBox(height: 30,),
-          SettingCellText(
-            text: Text(S.of(context).switch_account,style: TextStyle(fontSize: 16),),
-            onPressed: () => AppNavigate.push(AccountSwitch(),routeType: RouterType.material),
+          SizedBox(
+            height: 30,
           ),
-          SizedBox(height: 10,),
           SettingCellText(
-            text: Text(S.of(context).sign_out,style: TextStyle(fontSize: 16),),
+            text: Text(
+              S.of(context).switch_account,
+              style: TextStyle(fontSize: 16),
+            ),
+            onPressed: () => AppNavigate.push(AccountSwitch(),
+                routeType: RouterType.material),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SettingCellText(
+            text: Text(
+              S.of(context).sign_out,
+              style: TextStyle(fontSize: 16),
+            ),
             onPressed: _onExitPressed,
           ),
         ],
